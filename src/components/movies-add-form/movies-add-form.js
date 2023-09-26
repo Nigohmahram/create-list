53fc1510b93119ee80252709bffe890b4f93313f
Usmonov Hojiakbar <usmonovmaxsudovich@gmail.com>
2023-09-27 01:55:05 +0900

    input forms

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,19 +1,49 @@
+import { Component } from 'react';
 import './movies-add-form.css';
 
-const MoviesAddForm = () => {
-	return (
-		<div className='app-add-form'>
-			<h3>새 영화 추가</h3>
-			<form className='add-form d-flex'>
-				<input type='text' className='form-control new-post-label' placeholder='무슨 영화?' />
-				<input type='number' className='form-control new-post-label' placeholder='몇 번이나 조회되었나요?' />
+class MoviesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			views: '',
+		};
+	}
+	changeHandleInput = e => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+	render() {
+		const { name, views } = this.state;
+		return (
+			<div className='app-add-form'>
+				<h3>새 영화 추가</h3>
+				<form className='add-form d-flex'>
+					<input
+						type='text'
+						className='form-control new-post-label'
+						placeholder='무슨 영화?'
+						onChange={this.changeHandleInput}
+						name='name'
+						value={name}
+					/>
+					<input
+						type='number'
+						className='form-control new-post-label'
+						placeholder='몇 번이나 조회되었나요?'
+						onChange={this.changeHandleInput}
+						name='views'
+						value={views}
+					/>
 
-				<button type='submit' className='btn btn-outline-dark'>
-					추가
-				</button>
-			</form>
-		</div>
-	);
-};
+					<button type='submit' className='btn btn-outline-dark'>
+						추가
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default MoviesAddForm;
